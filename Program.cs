a2ae5ad1488144fb8bbe5be1dcd6a244667462a5
elena <elena@DESKTOP-PG6RJ3Q>
2023-03-27 00:55:21 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace quize1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int d = 44;
+            int e = 22;
+            int f = (e + d) / 2;
+            int g = d / e;           
+            int h = d / 2;
+            int j = e / 2;
+
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+            Console.WriteLine(j);
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
